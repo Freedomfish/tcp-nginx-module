ffb452cc1a1d57e3119e95c66d609145092c387a
Kenshin <xiyingit@gmail.com>
2014-03-24 17:35:45 +0800

    Update pp_cmdso.h

--- ping_pong/svr/pp_cmdso.h
+++ ping_pong/svr/pp_cmdso.h
@@ -1,4 +1,3 @@
-
 #ifndef _PP_CMDSO_H_INCLUDED_
 #define _PP_CMDSO_H_INCLUDED_
 
@@ -7,7 +6,7 @@
 #define PP_CMD_CS 100
 #define PP_CMD_SC 101
 
-long cmdso_load(void *cycle_param, cmd_pkg_handler_add_pt add_h, int slot);
+long cmdso_load(void *cycle_param, cmd_pkg_handler_add_pt add_h, int slot, ngx_tcp_global_ctx_t *load_ctx);
 long cmdso_unload(void *cycle_param);
 long cmdso_sess_init(ngx_tcp_ctx_t *ctx);
 long cmdso_sess_finit(ngx_tcp_ctx_t *ctx);
